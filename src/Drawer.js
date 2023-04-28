772730972a118599dfb4a78e0949c4d4305f6f34
OlgaNovik1 <7755517@gmail.com>
2023-04-28 16:14:07 +0300

    3

---
+++ src/Drawer.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+function Drawer() {
+    return (
+        <div style={{ display: 'none' }} className="overlay">
+            <div className="drawer">
+                <h2 className="d-flex justify-between mb-30 ">
+                    Корзина <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="remove"></img>
+                </h2>
+                <div className="items flex">
+                    <div className="cardItem d-flex align-center mb-20">
+                        <div style={{ backgroundImage: 'url(img/sneakers/1.jpg)' }} className="cartItemImg"></div>
+                        <div className="mr-20 flex">
+                            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                            <b>12 999 руб.</b>
+                        </div>
+                        <img className="removeBtn" src="/img/btn-remove.svg" alt="remove"></img>
+                    </div>
+                    <div className="cardItem d-flex align-center mb-20">
+                        <div style={{ backgroundImage: 'url(img/sneakers/1.jpg)' }} className="cartItemImg"></div>
+                        <div className="mr-20 flex">
+                            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                            <b>12 999 руб.</b>
+                        </div>
+                        <img className="removeBtn" src="/img/btn-remove.svg" alt="remove"></img>
+                    </div>
+                    <div className="cardItem d-flex align-center mb-20">
+                        <div style={{ backgroundImage: 'url(img/sneakers/1.jpg)' }} className="cartItemImg"></div>
+                        <div className="mr-20 flex">
+                            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                            <b>12 999 руб.</b>
+                        </div>
+                        <img className="removeBtn" src="/img/btn-remove.svg" alt="remove"></img>
+                    </div>
+                </div>
+                <div className="cartTotalBlock">
+                    <ul >
+                        <li>
+                            <span>Итого:</span>
+                            <div></div>
+                            <b>21 498 руб. </b>
+                        </li>
+                        <li >
+                            <span>Налог 5%: </span>
+                            <div></div>
+                            <b>1074 руб. </b>
+                        </li>
+                    </ul>
+                    <button className="greenButton">
+                        Оформить заказ <img src="/img/arrow.svg" alt="arrow"></img>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Drawer
